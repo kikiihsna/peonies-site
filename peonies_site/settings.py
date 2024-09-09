804ba559ec83b8260326ed67066f1ac801c4a785
kikiihsna <rizki.amani@ui.ac.id>
2024-09-09 16:12:54 +0700

    add url pws di settings.py

--- peonies_site/settings.py
+++ peonies_site/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-h4&#%)p=tt6-)-!4226&scic-(l=n+fmc-hrye0%ly9@5^!n&-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "rizki-amani-peoniessite.pbp.cs.ui.ac.id"]
 
 
 # Application definition
